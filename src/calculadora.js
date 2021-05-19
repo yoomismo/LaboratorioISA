2163b9c3d2aec4c68b3b95d887e0201bf1600d47
Yo <a@a.e>
2021-05-19 18:24:01 +0200

    sin conflictos

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-let suma;
-//Suma andres
- 
-
-//resta andres
-
-//multipliacopn Anyel
- 
-
-//Division anyel
-
-//raiz cuadrada juan
-=======
 let a = 5;
 let b = 15;
 
 const suma = () => {
+
     const resultado = suma(a,b)
     console.assert(resultado === a+b, "ha fallado")
 }
 
 suma();
->>>>>>> 648bac741e2710e66f9f7916338d2a724fc51898
