3bc63a02e158c70cd58339adc45735d24c191203
git config --global user.name Anyel <anyel.martich@gmail.com>
2021-05-19 18:38:32 +0200

    ?

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let suma;
 //Suma andres
  
@@ -10,15 +9,4 @@ let suma;
 
 //Division anyel
 
-//raiz cuadrada juan
-=======
-let a = 5;
-let b = 15;
-
-const suma = () => {
-    const resultado = suma(a,b)
-    console.assert(resultado === a+b, "ha fallado")
-}
-
-suma();
->>>>>>> 648bac741e2710e66f9f7916338d2a724fc51898
+//raiz cuadrada juang
